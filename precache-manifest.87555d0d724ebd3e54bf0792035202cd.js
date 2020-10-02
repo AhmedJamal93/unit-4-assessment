254905ed400050fece42527f2cc10569f16b1fa5
User <user@Users-MacBook-Pro.local>
2020-10-02 15:50:03 -0400

    Updates

---
+++ precache-manifest.87555d0d724ebd3e54bf0792035202cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9033ece832814d81a9051f761f7099",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "35d0250a23c7e8cc6c26",
+    "url": "/project-4-assessment/static/css/main.d65befc5.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d0250a23c7e8cc6c26",
+    "url": "/project-4-assessment/static/js/main.ed747e2f.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
